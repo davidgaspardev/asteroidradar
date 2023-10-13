4566718fc514890b00adef09ef8f0daa8a47e979
David Gaspar <davidgaspar.dev@gmail.com>
2023-10-12 21:31:18 -0300

    refactor: use indexer access for get ViewModel

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -10,11 +10,14 @@ import com.udacity.asteroidradar.databinding.FragmentMainBinding
 class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
+        ViewModelProvider(this)[MainViewModel::class.java]
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
