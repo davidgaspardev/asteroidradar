d4d11e8959803b7c3a00e22fa140798734c7b500
David Gaspar <davidgaspar.dev@gmail.com>
2024-04-08 23:50:25 -0300

    feat: implement picture of day with title name

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -6,9 +6,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.squareup.picasso.Picasso
 import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.R
-import com.udacity.asteroidradar.asteroids
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -38,6 +38,12 @@ class MainFragment : Fragment() {
                 asteroids.clear()
                 asteroids.addAll(asteroidList)
                 asteroidListAdapter.notifyDataSetChanged()
+            }
+        }
+
+        viewModel.pictureOfDay.observe(viewLifecycleOwner) {pictureOfDay ->
+            pictureOfDay?.let {
+                Picasso.get().load(pictureOfDay.url).into(binding.activityMainImageOfTheDay)
             }
         }
 
--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,15 +1,19 @@
 package com.udacity.asteroidradar.main
 
+import android.icu.util.Calendar
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.Constants.API_QUERY_DATE_FORMAT
+import com.udacity.asteroidradar.Constants.DEFAULT_END_DATE_DAYS
+import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.api.AsteroidApi
-import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
 import kotlinx.coroutines.launch
 import java.lang.Exception
+import java.text.SimpleDateFormat
 
 class MainViewModel : ViewModel() {
 
@@ -20,10 +24,21 @@ class MainViewModel : ViewModel() {
     private fun loadAsteroid() {
         viewModelScope.launch {
             try {
-                val asteroidFeed = AsteroidApi.asteroidService.getFeedByRange("2023-11-01", "2023-11-07")
-                Log.d(LOG_TAG, asteroidFeed.getData().toString())
+                val pictureOfDay = AsteroidApi.service.getPictureOfDay()
+                Log.d(LOG_TAG, "PICTURE OF DAY: $pictureOfDay")
+
+                val today = Calendar.getInstance()
+                val dateEnd = SimpleDateFormat(API_QUERY_DATE_FORMAT).format(today.time)
+                today.add(Calendar.DAY_OF_YEAR, -DEFAULT_END_DATE_DAYS) // Subtract 7 days
+                val dateStart = SimpleDateFormat(API_QUERY_DATE_FORMAT).format(today.time)
+
+                Log.d(LOG_TAG, "DATE START: $dateStart")
+                Log.d(LOG_TAG, "DATE END: $dateEnd")
+
+                val asteroidFeed = AsteroidApi.service.getFeedByRange(dateStart, dateEnd)
 
                 _asteroidList.value = asteroidFeed.getData()
+                _pictureOfDay.value = pictureOfDay
             } catch (err: Exception) {
                 Log.e(LOG_TAG, err.toString())
             }
@@ -33,6 +48,10 @@ class MainViewModel : ViewModel() {
     private val _asteroidList = MutableLiveData<List<Asteroid>>()
     val asteroidList: LiveData<List<Asteroid>>
         get() = _asteroidList
+
+    private val _pictureOfDay = MutableLiveData<PictureOfDay>()
+    val pictureOfDay: LiveData<PictureOfDay>
+        get() = _pictureOfDay
 
     private val _navigateToAsteroidDetail = MutableLiveData<Asteroid?>()
     val navigateToAsteroidDetail: LiveData<Asteroid?>
