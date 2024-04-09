a310c0af0c84ee97525ba6fa9560220da0a177b7
David Gaspar <davidgaspar.dev@gmail.com>
2024-04-08 23:48:52 -0300

    feat: add API call to get picture of day

--- app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
@@ -3,6 +3,7 @@ package com.udacity.asteroidradar.api
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.udacity.asteroidradar.Constants
+import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.api.dto.AsteroidFeed
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -11,15 +12,6 @@ import retrofit2.http.Query
 
 private const val TOKEN = "albfdeCpbixPOOkRicEbYNQNdBP3Wzb1Yl19A6sg"
 
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    .baseUrl(Constants.BASE_URL)
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .build()
-
 interface AsteroidApiService {
     @GET("neo/rest/v1/feed")
     suspend fun getFeedByRange(
@@ -27,10 +19,28 @@ interface AsteroidApiService {
         @Query("end_date") dateEnd: String,
         @Query("api_key") apiKey: String = TOKEN
     ): AsteroidFeed
+
+    @GET("planetary/apod")
+    suspend fun getPictureOfDay(
+        @Query("api_key") apiKey: String = TOKEN
+    ): PictureOfDay
 }
 
 object AsteroidApi {
-    val asteroidService: AsteroidApiService by lazy {
+    private val retrofit: Retrofit by lazy {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        retrofit
+    }
+
+    val service: AsteroidApiService by lazy {
         retrofit.create(AsteroidApiService::class.java)
     }
 }
